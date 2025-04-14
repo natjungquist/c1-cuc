ba3699f11ffe759e02e47f4b9ebf979321e87ccd
njungquist <njungquist@onec1.com>
2025-04-14 11:44:40 -0700

    code to get business hours keys and audio file path done

--- CallHandler.py
+++ CallHandler.py
@@ -2,20 +2,25 @@
 
 class CallHandler:
 
-  def __init__(self, name):
+  def __init__(self, data):
+    
+    # from config file
+    self.Name = data['Name']
+    self.Site = data['Site']
+    self.PilotIdentifierList = data['PilotIdentifierList']
+    self.OperatorExtension = data['OperatorExtension']
+    self.BusinessHoursWelcomeGreetingFilename = data['BusinessHoursWelcomeGreetingFilename']
+    self.BusinessHoursMainMenuCustomPromptFilename = data['BusinessHoursMainMenuCustomPromptFilename']
+    self.BusinessHoursKeyMapping = data['BusinessHoursKeyMapping']                  
+    self.BusinessHoursKeyMappingEnabled = True if data['BusinessHoursKeyMappingEnabled'] == 'TRUE' else False        
+    self.AfterHoursWelcomeGreetingFilename = data['AfterHoursWelcomeGreetingFilename']       
+    self.AfterHoursMainMenuCustomPromptFilename = data['AfterHoursMainMenuCustomPromptFilename']
+    self.AfterHoursKeyMapping = data['AfterHoursKeyMapping']                         
+    self.AfterHoursKeyMappingEnabled = True if data['AfterHoursKeyMappingEnabled'] == 'TRUE' else False
+
+    # other
     self.UnityId = ""     # id in CUC
-    self.Name = name
-    self.Site = ""
-    self.PilotIdentifierList = 0
-    self.OperatorExtension = 0
-    self.BusinessHoursWelcomeGreetingFilename = ""
-    self.BusinessHoursMainMenuCustomPromptFilename = "" 
-    self.BusinessHoursKeyMapping = ""                    
-    self.BusinessHoursKeyMappingEnabled = ""                
-    self.AfterHoursWelcomeGreetingFilename = ""             
-    self.AfterHoursMainMenuCustomPromptFilename = ""        
-    self.AfterHoursKeyMapping = ""                          
-    self.AfterHoursKeyMappingEnabled = "" 
+    self.prefix = self.Name[0:3]
 
   def get_id(self):
     return self.UnityId
